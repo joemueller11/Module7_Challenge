acafef97cb9814a342e386210a14a91487818d85
Joseph Mueller <joseph.m.email@gmail.com>
2025-02-02 05:10:42 -0500

    questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,58 @@
 // TODO: Include packages needed for this application
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js'; 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please provide a description of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide installation instructions.',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide usage information.',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please provide contribution guidelines.',
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Please provide test instructions.',
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license.',
+        choices: ['MIT', 'GNU GPLv3', 'Apache 2.0', 'ISC'],
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please provide your GitHub username.',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please provide your email address.',
+    },
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
